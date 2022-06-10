cb7adf74c48002a87c47e2bc50b80b80f412776d
doofin <8177dph@gmail.com>
2022-06-10 19:36:57 +0200

    add await

--- jvm/src/main/scala/com/doofin/stdScala/jvmUtils.scala
+++ jvm/src/main/scala/com/doofin/stdScala/jvmUtils.scala
@@ -4,8 +4,12 @@ package com.doofin.stdScala
 
 import java.io.PrintWriter;
 import java.io.File;
+import scala.concurrent.{Await, ExecutionContext, Future, duration}
+import scala.concurrent.duration.FiniteDuration
 
 trait jvmUtils {
+  def command(value: String) = sys.process.stringToProcess(value).!!
+  // val secondsDuration: Int => FiniteDuration = x => x.seconds
   def writeToFile(url: String, s: String) = {
     val pw = new PrintWriter(new File(url))
     pw.println(s)
@@ -19,4 +23,5 @@ trait jvmUtils {
     s
   }
 
+  def awaitFuture[t](x: Future[t]): t = Await result (x, duration.Duration.Inf)
 }
